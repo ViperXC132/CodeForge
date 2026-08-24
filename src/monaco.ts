70d09e4c72ec2285ef31a2165f3212af6a549f29
Viper Davis <viperxc2204@gmail.com>
2026-08-24 21:27:53 +0100

    fix: use typed Monaco worker environment

--- src/monaco.ts
+++ src/monaco.ts
@@ -5,7 +5,7 @@ import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
 import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 
-self.MonacoEnvironment = {
+window.MonacoEnvironment = {
   getWorker(_workerId: string, label: string) {
     if (label === 'json') return new jsonWorker()
     if (label === 'css' || label === 'scss' || label === 'less') return new cssWorker()
